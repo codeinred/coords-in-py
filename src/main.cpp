a64af7941c9628c213f8d97bc609abdc7404005e
Alecto Irene Perez <perez.cs@pm.me>
2022-09-29 10:35:26 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <vector>
+
+namespace py = pybind11;
+
+struct Point {
+    long x, y;
+};
+
+std::vector<Point> solve(int a, int b, int c, int d) {
+    std::vector<Point> vect;
+    // Code goes here
+
+
+    return vect;
+}
+
+// NOTE: The name given here must match the one given in CMakeLists.txt
+PYBIND11_MODULE(coords, m) {
+    // Declare the point class
+    py::class_<Point>(m, "Point")
+        .def(py::init<long, long>()) // Point takes 2 longs to construct
+        .def_readonly("x", &Point::x)
+        .def_readonly("y", &Point::y);
+
+    // Declare the solve function
+    m.def(
+        "solve", // Name in python
+        &solve,  // Address of function
+        "Returns a vector of points containing the solution");
+}
